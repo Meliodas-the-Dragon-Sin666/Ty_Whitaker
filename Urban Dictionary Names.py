f89796bcbddab69fd76ca6c4b967a7898291bbeb
Tyrese Whitaker <tyrese.whitaker@stu.jefferson.kyschools.us>
2018-03-20 07:38:41 -0400

    first commit

---
+++ Urban Dictionary Names.py
@@ -0,0 +1,17 @@
+Urban Dictionary Names:
+
+
+
+    Chris: He is very energetic. He always knows whats he is doing help others with their problem, a very loyal person always be there when you need him.
+
+    Baylee: A beautiful girl with and even better personality, she has a fantastic taste in music and always looks amazing in whatever she wears.
+
+    Morgan: A person who is beautiful, but they never believe it themselves. They are caring and always there for their friends, and always trying to solve everyone else's problems.
+
+    Shelby: Shelby is unlike any other. She also goes by the name Shelbster. She is perfect in every way, she's the best in the world but she doesn't think so.
+
+    Tory: A funny, smart, sweet, sexy and caring girl who loves with all her heart. She's also a crazy, psycho bitch when you mess with someone she cares about. 
+
+    Jose: Yes, it's one of the most hispanic common names. Besides that, its the definition of a guy who is beyond amazing, handsome, sweet, adorable, caring, funny, dorky and much more. 
+
+    
