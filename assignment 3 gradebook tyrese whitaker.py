f57ae89c8f20a3413500aa58198ad364b5a1a325
Meliodas the Dragon Sin <36765273+Meliodas-the-Dragon-Sin666@users.noreply.github.com>
2018-03-28 07:45:41 -0400

    Add files via upload

---
+++ assignment 3 gradebook tyrese whitaker.py
@@ -0,0 +1,19 @@
+input grade:
+print grade if below 60
+55
+
+input grade:
+print grade if between 60 and 70
+68
+
+input grade:
+print grade if between 70 and 80
+71
+
+input grade:
+print grade if between 80 and 90
+89
+
+input grade:
+print grade if between 90 and 100
+100
