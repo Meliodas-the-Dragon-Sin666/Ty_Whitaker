18f52c9ef2ad01c1198ab4901eff4c201007538c
Meliodas the Dragon Sin <36765273+Meliodas-the-Dragon-Sin666@users.noreply.github.com>
2018-05-02 08:08:09 -0400

    second commit

---
+++ computer project.py
@@ -0,0 +1,56 @@
+def pet_dogs if favorite breed
+keep_walking if not favorite breed
+
+pet_GermanShepherd = true
+else keep_walking
+
+pet_Chihuahua = false
+else keep_walking
+
+pet_Rottweiler = true
+else keep_walking
+
+pet_GoldenRetriever = true
+else keep_walking
+
+pet_SiberianHusky = true
+else keep_walking
+
+pet_YorkshireTerrier = false
+else keep_walking
+
+pet_Doberman = true
+else keep_walking
+
+pet_GreatDane = true
+else keep_walking
+
+pet_EnglishMastiff = true
+else keep_walking
+
+pet_Greyhound = false
+else keep_walking
+
+pet_BorderCollie = true
+else keep_walking
+
+pet_Pitbull = true
+else keep_walking
+
+pet_AustralianShepherd = true
+else keep_walking
+
+pet_ShihTzu = false
+else keep_walking
+
+pet_St.Bernard = true
+else keep_walking
+
+pet_BerneseMountainDog = true
+else keep_walking
+
+pet_ShetlandSheepDog = true
+else keep_walking
+
+pet_Pomeranian = false
+else keep_walking
